57b88d8f7ab7109e055e06752fc0690d61d48521
Lora Lin <Lora_Lin@wiwynn.com>
2021-11-02 04:22:54 -0400

    fby3.5: cl: Add snoop read
    Summary:
    - Add snoop read for getting post code.
    - Impletmented get post code command.
    - Fix ISR for post code complete status.
    
    Test plan:
    -Build code: Pass
    -Get post code: Pass
    
    Log:
    root@bmc-oob:~# bic-util slot1 0xe0 0x12 0x9c 0x9c 0
    9C 9C 00 A7 A9 A9 A2 A2 A7 A7 A7 A9 A9 A8 AA AE
    E0 E0 E0 E1 E4 E3 E5 AF AF B0 B5 7E CF 7E 73 CD
    B0 7E C1 70 B1 B1 7E C2 7E 70 7E 7E B1 B1 B1 7E
    B4 7E B8 C5 B2 C6 B3 B3 B3 B3 B3 B6 B6 B6 B0 B7
    B6 B6 7E 7E B7 B7 B6 B6 B7 B7 7E 70 7E 70 70 B7
    7E BE BE 7E 7E D2 7E D2 D6 70 B9 B7 B7 B7 B7 B7
    B7 B8 B8 B8 B8 B8 B8 B8 B8 B8 D7 C9 DA D9 DB BA
    B9 70 70 70 7E CB BB BB BB BB BB BB BB BB BB BB
    7E 7E D0 7E D0 7E D0 7E D1 7E D1 7E 70 7E B7 CA
    CA DC 7E CC BC BC BC BC BC CE C6 7E BF AF E6 E7
    E9 EB EC ED EE 02 22 50 00 04 06 0B 0C 0D 15 7F
    00 7F 40 41 42 47 4F 61 68 70 79 91 92 94 94 94
    94 94 94 94 94 94 94 94 94 94 94 94 94 95 96 EF
    92 92 92 92 92 92 92 92 99 91 92 92 92 92 92 92
    92 92 92 97 98 92 99 92 92 92 92 92 92 92 92

--- meta-facebook/yv35-cl/src/platform/hwmon.c
+++ meta-facebook/yv35-cl/src/platform/hwmon.c
@@ -1,5 +1,6 @@
 #include "plat_func.h"
 #include "plat_gpio.h"
+#include "hal_snoop.h"
 
 static bool is_DC_on = 0;
 static bool is_post_complete = 0;
@@ -19,6 +20,12 @@ void ISR_DC_on() {
 void set_DC_status() {
   is_DC_on = gpio_get(PWRGD_SYS_PWROK);
   printk("set dc status %d\n", is_DC_on);
+
+  if ( is_DC_on ){
+    snoop_start_thread();
+  }else{
+    free_snoop_buffer();
+  }
 }
 
 bool get_DC_status() {
@@ -28,6 +35,10 @@ bool get_DC_status() {
 void set_post_status() {
   is_post_complete = !(gpio_get(FM_BIOS_POST_CMPLT_BMC_N));
   printk("set is_post_complete %d\n", is_post_complete);
+
+  if ( is_post_complete ){
+    snoop_abort_thread();
+  }
 }
 
 bool get_post_status() {
